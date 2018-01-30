b801986d964ce8f5b94fe0063f3ac60155ccca72
dencaval <dencaval@gmail.com>
2018-01-30 10:49:16 -0300

    Add first version of modified index handle in order to parse cosbench workload/stages

--- dev/cosbench-controller-web/src/com/intel/cosbench/controller/handler/IndexHandler.java
+++ dev/cosbench-controller-web/src/com/intel/cosbench/controller/handler/IndexHandler.java
@@ -17,10 +17,19 @@ limitations under the License.
 
 package com.intel.cosbench.controller.handler;
 
+import java.io.StringWriter;
+import java.io.Writer;
+
 import javax.servlet.http.*;
 
 import com.intel.cosbench.model.*;
 import com.intel.cosbench.service.ControllerService;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonWriter;
 
 public class IndexHandler extends AbstractClientHandler {
 
@@ -29,34 +38,49 @@ public class IndexHandler extends AbstractClientHandler {
     public void setController(ControllerService controller) {
         this.controller = controller;
     }
-
+    
     @Override
     protected String process(HttpServletRequest req, HttpServletResponse res)
             throws Exception {
-        StringBuilder buffer = new StringBuilder();
-        DriverInfo[] drivers = controller.getControllerInfo().getDriverInfos();
-        buffer.append("Drivers:").append('\n');
-        for (DriverInfo driver : drivers)
-            buffer.append(driver.getName()).append('\t')
-                    .append(driver.getUrl()).append('\n');
-        buffer.append("Total:").append(' ').append(drivers.length).append(' ')
-                .append("drivers").append('\n');
-        buffer.append('\n');
-        WorkloadInfo[] workloads = controller.getActiveWorkloads();
-        buffer.append("Active Workloads:").append('\n');
-        for (WorkloadInfo workload : workloads) {
-            buffer.append(workload.getId()).append('\t')
-                    .append(workload.getSubmitDate()).append('\t')
-                    .append(workload.getState()).append('\t');
-                    if (workload.getCurrentStage() != null) {
-                    	buffer.append(workload.getCurrentStage().getId()).append('\n');	
-                    } else {
-                    	buffer.append("None").append('\n');
-                    }
-        }
-        buffer.append("Total:").append(' ').append(workloads.length)
-                .append(' ').append("active workloads").append('\n');
-        return buffer.toString();
+    	DriverInfo[] drivers = controller.getControllerInfo().getDriverInfos();
+    	JsonArrayBuilder json_drivers = Json.createArrayBuilder();
+    	for (DriverInfo driver : drivers){
+    		JsonObjectBuilder json_driver = Json.createObjectBuilder();
+    		json_driver.add("id", driver.getName());
+    		json_driver.add("url", driver.getUrl());  		
+    		json_drivers.add(json_driver.build());
+    	}
+    	
+    	WorkloadInfo[] workloads = controller.getActiveWorkloads();
+    	JsonArrayBuilder json_workloads = Json.createArrayBuilder();
+    	for (WorkloadInfo workload : workloads){
+    		JsonObjectBuilder json_workload = Json.createObjectBuilder();
+    		json_workload.add("id", workload.getId());
+    		json_workload.add("status", workload.getState().toString());
+    		
+    		StageInfo[] stages = workload.getStageInfos();
+        	JsonArrayBuilder json_stages = Json.createArrayBuilder();
+        	for (StageInfo stage : stages){
+        		JsonObjectBuilder json_state = Json.createObjectBuilder();
+        		json_state.add("id", stage.getId());
+        		json_state.add("name", stage.getStage().getName());
+        		json_state.add("status", stage.getState().name().toString());
+        		
+        		json_stages.add(json_state.build());    		
+        	}
+        	
+        	json_workload.add("stages", json_stages.build());        	
+        	json_workloads.add(json_workload.build());    		
+    	}
+    	
+    	JsonObjectBuilder data = Json.createObjectBuilder();
+    	data.add("drivers", json_drivers.build());
+    	data.add("workloads", json_workloads.build());
+    	
+    	StringWriter stringWriter = new StringWriter();
+    	JsonWriter writer = Json.createWriter(stringWriter);
+        writer.writeObject(data.build());
+        writer.close();
+        return stringWriter.toString();
     }
-
 }
